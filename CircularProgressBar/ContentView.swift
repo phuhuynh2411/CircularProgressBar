ddfc4b839bbe4cc05338a2fd37318f262da9fb35
Phu Huynh <phu@filesoft.tech>
2020-03-09 13:51:16 +0700

    * Complete the progress bar

--- CircularProgressBar/ContentView.swift
+++ CircularProgressBar/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var circleProgress: CGFloat = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 15)
+                    .foregroundColor(.gray)
+                    .frame(width: 300, height: 300)
+                
+                Circle()
+                    .trim(from: 0, to: circleProgress)
+                    .stroke(lineWidth: 15)
+                    .rotation(.degrees(-90))
+                    .foregroundColor(.blue)
+                    .frame(width: 300, height: 300)
+                
+                Text(" \(Int(self.circleProgress * 100)) %")
+                    .font(.system(size: 20))
+                   
+            }
+            
+            Button(action: { self.startTimer() }) {
+                Text("Start Timer")
+            }
+        }
+    }
+    
+    func startTimer() {
+        let _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+            withAnimation {
+                if self.circleProgress >= 1.0 {
+                    timer.invalidate()
+                } else {
+                    self.circleProgress += 0.01
+                }
+            }
+        }
     }
 }
 
